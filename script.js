f20323f17190e246f7cfd48abfe609c593185d55
studio4627 <yeoulab@gmail.com>
2020-11-28 12:21:25 +0900

    20201128
    gradient practice

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-int i = 0;
 
-document.getElementById("here").innerHTML = i;
+var i = 40;
+var bg = document.getElementById("background");
+bg.style.background = "linear-gradient(25deg, rgba(240,240,240,1) 0%, rgba(190,120,210,1) 100%)";
+//bg.style.background = "linear-gradient(25deg, rgba(0,0,0,1) 0%, rgba(70,0,90,1) 100%)";
+//왼쪽 아래는 4씩, 오른쪽 위는 2씩 증가
+var str_intro = "그들의 기구와 장비는<br />"+
+                "모두 살아있다."
+document.getElementById("intro_string").innerHTML = str_intro;
